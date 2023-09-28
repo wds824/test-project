9babb939d5d49090f0a70aee676c3c903c4f9dd6
Base64 <wds8.24@qq.com>
2023-09-28 22:19:24 +0800

    update project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,108 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="grid-container">
+    <div class="header">
+        header
+    </div>
+    <div class="menu">
+        menu
+    </div>
+    <div class="aside">
+        aside
+    </div>
+    <div class="booter">
+        booter
+    </div>
+
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'HelloWorld'
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+* {
+            margin: 0;
+            padding: 0;
+        }
+
+        html {
+            min-height: 100%;
+        }
+
+        body {
+            min-height: 100%;
+            position: relative;
+        }
+
+        body::before {
+            z-index: -1;
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            min-height: 100%;
+            background-size: cover;
+            background: url('../assets/background.jpg') repeat-y;
+            content: '';
+            display: block;
+            filter: blur(30px);
+        }
+
+        .grid-container {
+            margin: 20px;
+
+            display: grid;
+            grid-template-areas:
+            'header  header header'
+            'menu aside aside'
+            'booter booter booter';
+            grid-column-gap: 20px;
+            grid-row-gap: 20px;
+        }
+
+        @media screen and (max-width: 700px) {
+            .grid-container {
+                 grid-template-areas:
+                'header header header'
+                'aside aside aside'
+                'booter booter booter';
+            }
+            .menu{
+                display: none !important;
+            }
+
+        }
+
+        .header {
+            grid-area: header;
+            height: 80px;
+
+        }
+
+        .menu {
+            grid-area: menu;
+            height: 600px;
+        }
+
+        .aside {
+            grid-area: aside;
+            height: 600px;
+        }
+
+        .booter {
+            grid-area: booter;
+            height: 80px;
+        }
+
+        .header, .menu, .aside, .booter {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: white;
+            opacity: .8;
+            border-radius: 10px;
+        }
 </style>
